63cd4a4638a16b4b61d2eaf67c80f607f2dee12c
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2020-11-25 02:33:31 +0300

    Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import IUserState from "../../redux/model/IUserState";
+import HomeStudent from "./HomeStudent";
+import HomeTeacher from "./HomeTeacher";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { user, isLoggedIn } = useSelector((state: IUserState) => ({
+    user: state.user,
+    isLoggedIn: state.isLoggedIn,
+  }));
+
+  if (isLoggedIn === "STUDENT") {
+    return <HomeStudent />;
+  } else if (isLoggedIn === "TEACHER") {
+    return <HomeTeacher />;
+  }
+  return <div>Welcome {user.username}</div>;
 };
 
 export default Home;
