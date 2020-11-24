84b1b907c0d9abc0c8582b64c9694c66a9377764
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2020-11-25 02:35:19 +0300

    redux reducer

--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -1,11 +1,53 @@
-import { combineReducers } from "redux";
-import ICombinedState from "../model/ICombinedState";
+import { Reducer } from "redux";
 import IAction from "../model/IAction";
+import IUserState, { initialUserState } from "../model/IUserState";
+import * as USER_ACTIONS from "../actionType/user";
 
-import user from "./user";
-import student from "./student";
-import teacher from "./teacher";
+const userReducer: Reducer<IUserState, IAction> = (state: IUserState = initialUserState, action: IAction): IUserState => {
+  switch (action.type) {
+    case USER_ACTIONS.LOGIN_FINALLY:
+      return {
+        ...state,
+        isLoggedIn: action.payload,
+      };
 
-const rootReducer = combineReducers<ICombinedState, IAction>({ user, student, teacher });
+    case USER_ACTIONS.LOGOUT_FINALLY:
+      return initialUserState;
 
-export default rootReducer;
+    case USER_ACTIONS.CHANGE_USER:
+      return {
+        ...state,
+        user: action.payload,
+      };
+
+    case USER_ACTIONS.SAVE_TEACHER:
+      const teachers = state.teachers;
+      teachers.push(action.payload);
+      return {
+        ...state,
+        teachers,
+      };
+
+    case USER_ACTIONS.SAVE_STUDENT:
+      const students = state.students;
+      students.push(action.payload);
+      return {
+        ...state,
+        students,
+      };
+
+    case USER_ACTIONS.DELETE_STUDENT:
+      let indexStudent = state.students.findIndex((student) => student.email === action.payload);
+      let studentList = state.students;
+      if (indexStudent > -1) {
+        studentList.splice(indexStudent, 1);
+      }
+      return {
+        ...state,
+        students: studentList,
+      };
+  }
+  return initialUserState;
+};
+
+export default userReducer;
