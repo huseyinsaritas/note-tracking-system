38beae160ab4d5f7794676c0b3b1adc45e69c5a4
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2020-11-25 02:35:35 +0300

    redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import thunk, { ThunkMiddleware } from "redux-thunk";
 
 import rootReducer from "./reducer";
-import ICombinedState from "./model/ICombinedState";
+import IUserState from "./model/IUserState";
 import IAction from "./model/IAction";
 
 const createAppStore = () => {
-  let storeEnhancer = applyMiddleware(thunk as ThunkMiddleware<ICombinedState, IAction>);
+  let storeEnhancer = applyMiddleware(thunk as ThunkMiddleware<IUserState, IAction>);
 
   const store = createStore(rootReducer, storeEnhancer);
   return store;
