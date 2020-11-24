15d8856e7d7c2a75796b69fbf8ac6e95b6257b99
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2020-11-25 02:34:44 +0300

    redux action

--- src/redux/action/user.ts
+++ src/redux/action/user.ts
@@ -1,7 +1,35 @@
+import IUser from "../../model/IUser";
+import IStudent from "../../model/IStudent";
+import ITeacher from "../../model/ITeacher";
 import * as USER_ACTIONS from "../actionType/user";
 import IAction from "../model/IAction";
 
-export const logged = (information: boolean): IAction => ({
-  type: USER_ACTIONS.LOGGED,
-  payload: information,
+export const changeLogin = (profession?: "TEACHER" | "STUDENT"): IAction => ({
+  type: USER_ACTIONS.LOGIN_FINALLY,
+  payload: profession,
 });
+
+export const changeUser = (user: IUser): IAction => ({
+  type: USER_ACTIONS.CHANGE_USER,
+  payload: user,
+});
+
+export const saveStudent = (student: IStudent): IAction => ({
+  type: USER_ACTIONS.SAVE_STUDENT,
+  payload: student,
+});
+
+export const saveTeacher = (teacher: ITeacher): IAction => ({
+  type: USER_ACTIONS.SAVE_TEACHER,
+  payload: teacher,
+});
+
+export const deleteStudent = (email: string): IAction => ({
+  type: USER_ACTIONS.DELETE_STUDENT,
+  payload: email,
+});
+
+// export const updateStudent = (clas: string): IAction => ({
+//   type: USER_ACTIONS.UPDATE_STUDENT,
+//   payload: clas,
+// });
