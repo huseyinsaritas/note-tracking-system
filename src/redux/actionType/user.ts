1aaa00cd6bdeb60e06fe94c698e320989bae2c5a
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2020-11-25 02:34:55 +0300

    redux actionType

--- src/redux/actionType/user.ts
+++ src/redux/actionType/user.ts
@@ -1,2 +1,7 @@
-export const LOGGED: string = "LOGGED";
-// export const LOGOUT_FINALLY: string = 'LOGOUT_FINALLY';
+export const LOGIN_FINALLY: string = "LOGIN_FINALLY";
+export const LOGOUT_FINALLY: string = "LOGOUT_FINALLY";
+export const CHANGE_USER: string = "CHANGE_USER";
+export const SAVE_STUDENT: string = "SAVE_STUDENT";
+export const SAVE_TEACHER: string = "SAVE_TEACHER";
+export const DELETE_STUDENT: string = "DELETE_STUDENT";
+// export const UPDATE_STUDENT: string = "UPDATE_STUDENT";
