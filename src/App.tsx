be51c0874762d74ccd18173ff514d826944223aa
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2020-11-25 02:35:47 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRouter from "./App/AppRouter/AppRouter";
 import "./App.css";
+import { students, teachers } from "./data/mockData";
+import { useDispatch } from "react-redux";
+import { saveStudent, saveTeacher } from "./redux/action/user";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    students.forEach((student) => {
+      dispatch(saveStudent(student));
+    });
+
+    teachers.forEach((teacher) => {
+      dispatch(saveTeacher(teacher));
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
       <AppRouter />
